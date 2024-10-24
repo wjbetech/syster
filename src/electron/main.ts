d299b4252fc1d80733e95f55b343c440e1c9a921
wjbetech <wjbetech@gmail.com>
2024-10-24 17:17:54 +0900

    electron initial setup complete

---
+++ src/electron/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from "electron";
+// full OS support via path
+import path from "path";
+
+type test = string;
+
+app.on("ready", () => {
+  const window = new BrowserWindow({
+    width: 800,
+    title: "Syster",
+    resizable: true,
+    center: true,
+    autoHideMenuBar: true,
+    backgroundColor: "#aaa",
+    roundedCorners: true,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  window.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+});
