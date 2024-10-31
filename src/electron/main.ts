056a34425f4c57ecc08ec558c02eb1a477a5c82c
wjbetech <wjbetech@gmail.com>
2024-10-31 22:17:56 +0900

    Added static and dynamic data utils for os-utils funcs

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,23 +2,9 @@ import { app, BrowserWindow } from "electron";
 // full OS support via path
 import path from "path";
 import { isDev } from "./utils/utils.js";
+import { pollResources } from "./utils/resourceManager.js";
 
 app.on("ready", () => {
-  // settings for syster electron window
-  const window = new BrowserWindow({
-    width: 800,
-    title: "Syster",
-    resizable: true,
-    center: true,
-    autoHideMenuBar: true,
-    backgroundColor: "#aaa",
-    roundedCorners: true,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-
   // dev vs prod toggle
   const mainWindow = new BrowserWindow({});
   if (isDev()) {
@@ -27,5 +13,5 @@ app.on("ready", () => {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
 
-  window.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+  pollResources();
 });
--- src/electron/utils/resourceManager.ts
+++ src/electron/utils/resourceManager.ts
@@ -1,5 +1,37 @@
+import osUtils from "os-utils";
+
 const POLLING_INTERVAL = 500;
 
 export function pollResources() {
-  setInterval(() => {}, POLLING_INTERVAL);
+  // grab static CPU data
+  getStaticCpuInfo();
+
+  // handle dynamic CPU data
+  setInterval(() => {
+    getDynamicInfo().then((info) => console.log(info));
+  }, POLLING_INTERVAL);
+}
+
+// static data
+function getStaticCpuInfo() {
+  const cpuPlatform = `System Platform: ${osUtils.platform()}`;
+  const coreCount = `Core Count : ${osUtils.cpuCount().toFixed(1)}`;
+  console.log(cpuPlatform + `\n` + coreCount + `\n` + `-----------------------`);
+  return { cpuPlatform, coreCount };
+}
+
+// dynamic data
+async function getDynamicInfo() {
+  // RAM
+  const totalMem = `${osUtils.totalmem().toFixed(1)}MB`;
+  const freeMem = `${osUtils.freemem().toFixed(1)}MB`;
+  const freeMemPercent = `${Number(osUtils.freememPercentage().toPrecision(1)) * 100}%`;
+
+  // CPU
+  const cpuUsage = await new Promise((resolve) => {
+    osUtils.cpuUsage((percentage) => {
+      resolve(`${Number(percentage.toPrecision(1)) * 100}%`);
+    });
+  });
+  return { freeMem, totalMem, freeMemPercent, cpuUsage };
 }
