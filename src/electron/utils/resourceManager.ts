7a43a86b86bf60a9891ae0aefc0d9b2a6d3bc8b9
wjbetech <wjbetech@gmail.com>
2024-11-03 19:52:15 +0900

    Fixing comments for legibility

--- src/electron/utils/resourceManager.ts
+++ src/electron/utils/resourceManager.ts
@@ -16,6 +16,7 @@ export function pollResources() {
 
 // static data
 function getStaticCpuInfo() {
+  // CPU
   const cpuPlatform = `System Platform: ${osUtils.platform()}`;
   const coreCount = `Thread Count : ${osUtils.cpuCount().toFixed(0)}`;
   const cpuModel = `CPU Model: ${os.cpus()[0].model}`;
@@ -28,7 +29,7 @@ async function getDynamicInfo() {
   // RAM
   const totalMem = `${(osUtils.totalmem() / 1024).toFixed(1)}GB`;
   const freeMem = `${(osUtils.freemem() / 1024).toFixed(1)}GB`;
-  const freeMemPercent = `${Number(osUtils.freememPercentage().toPrecision(1)) * 100}%`;
+  const freeMemPercent = `${Math.floor(Number(osUtils.freememPercentage().toPrecision(1)) * 100)}%`;
 
   // CPU
   const cpuUsage = await new Promise((resolve) => {
