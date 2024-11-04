5f1be24300d50465270e9466435bd68f4d45649f
wjbetech <wjbetech@gmail.com>
2024-11-05 02:50:37 +0900

    main and renderer communication running

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,10 @@
 import "./App.css";
 
 function App() {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  window.electron.getStaticData();
+
   return (
     <>
       <h1>hello world!</h1>
