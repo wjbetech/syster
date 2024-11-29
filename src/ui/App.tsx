f7b2563aca5ecf54942674cbd91c1d8e6df3a3ac
wjbetech <wjbetech@gmail.com>
2024-11-29 23:47:22 +0900

    Fixed transitioning sections in and out

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -13,6 +13,13 @@ function App() {
     ramUsage: true,
   });
 
+  // render state for transitions
+  const [isRendered, setIsRendered] = useState({
+    system: true,
+    cpuUsage: true,
+    ramUsage: true,
+  });
+
   const [staticData, setStaticData] = useState({
     cpuPlatform: "",
     coreCount: 0,
@@ -42,93 +49,139 @@ function App() {
     })();
   }, []);
 
-  // toggle visibility of specific elements
+  // handle visibility of elements with delays for smooth transitions
   const handleClick = (view: keyof typeof views) => {
-    setViews((prevViews) => ({
-      ...prevViews,
-      [view]: !prevViews[view],
-    }));
+    if (views[view]) {
+      setViews((prev) => ({
+        ...prev,
+        [view]: false,
+      }));
+
+      setTimeout(() => {
+        setIsRendered((prev) => ({
+          ...prev,
+          [view]: false,
+        }));
+      }, 500);
+    } else {
+      setIsRendered((prev) => ({
+        ...prev,
+        [view]: true,
+      }));
+      setTimeout(() => {
+        setViews((prev) => ({
+          ...prev,
+          [view]: true,
+        }));
+      }, 10);
+    }
   };
 
   return (
-    <div className="App p-4">
+    <div className="bg-slate-700 h-svh overflow-hidden p-2">
       <div>
         {/* header bar with minimize, maximize and close buttons */}
-        <header className="flex flex-row justify-between absolute top-0 left-0 w-full items-center p-2 box-border bg-gray-900">
-          <h1 className="font-bold text-2xl">Syster</h1>
-          <div className="flex flex-row gap-1">
+        <header className="flex flex-row justify-between absolute top-0 left-0 w-full items-center py-2 px-2 box-border bg-slate-800">
+          <h1 className="font-bold text-xl">Syster</h1>
+          <div className="flex flex-row">
             <button
               id="minimize"
               onClick={() => window.electron.sendFrameAction("MINIMIZE")}
-              className="bg-yellow-600 rounded-sm"
+              className="bg-yellow-600 rounded-sm scale-[0.80]"
             >
-              <img className="size-8" src={minimizeSymbol} alt="" />
+              <img className="size-8 scale-75" src={minimizeSymbol} alt="" />
             </button>
             <button
               id="maximize"
               onClick={() => window.electron.sendFrameAction("MAXIMIZE")}
-              className="bg-green-600 rounded-sm"
+              className="bg-green-600 rounded-sm scale-[0.80]"
             >
-              <img className="size-8" src={expandSymbol} alt="" />
+              <img className="size-8 scale-75" src={expandSymbol} alt="" />
             </button>
             <button
               id="close"
               onClick={() => window.electron.sendFrameAction("CLOSE")}
-              className="bg-red-600 rounded-sm"
+              className="bg-red-600 rounded-sm scale-[0.80]"
             >
-              <img className="size-8" src={closeSymbol} alt="" />
+              <img className="size-8 scale-75" src={closeSymbol} alt="" />
             </button>
           </div>
         </header>
 
         {/* system info (platform, CPU model, cores) */}
-        <section>
+        <section className="px-2 mt-12 mb-4">
           <h4
             onClick={() => handleClick("system")}
-            className="font-bold cursor-pointer"
+            className="font-bold cursor-pointer text-lg"
           >
             System{" "}
           </h4>
-          <div
-            id="system"
-            className={`transition-all duration-500 ease-in-out ${
-              views.system ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
-            } overflow-hidden`}
-          >
-            {views.system && (
+          {isRendered.system && (
+            <div
+              id="system"
+              className={`transition-all duration-500 ease-in-out ${
+                views.system ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+              } overflow-hidden`}
+            >
               <div className="text-sm">
                 <p>
-                  <span className="subheader">Platform: </span>
-                  {staticData.cpuPlatform === "win32" ? "Windows" : "Unknown"}
+                  <span className="text-slate-400 font-bold">Platform: </span>
+                  {staticData.cpuPlatform === "win32"
+                    ? "Windows"
+                    : `${staticData.cpuPlatform}`}
                 </p>
                 <p>
-                  <span className="subheader">CPU Model: </span>
+                  <span className="text-slate-400 font-bold">CPU Model: </span>
                   {staticData.cpuModel}
                 </p>
                 <p>
-                  <span className="subheader">Core Count: </span>
+                  <span className="text-slate-400 font-bold">Core Count: </span>
                   {staticData.coreCount}
                 </p>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </section>
       </div>
 
       {/* Toggle CPU usage chart */}
-      <div style={{ height: 160 }}>
-        <h4 id="chart-header-cpu" onClick={() => handleClick("cpuUsage")}>
+      <div className="my-4">
+        <h4
+          id="chart-header-cpu"
+          className="cursor-pointer mx-2 font-bold"
+          onClick={() => handleClick("cpuUsage")}
+        >
           CPU Usage
         </h4>
-        {views.cpuUsage && <Chart data={cpuUsage} maxDataPoints={10} />}
+        {isRendered.cpuUsage && (
+          <div
+            className={`transition-all duration-500 ease-in-out ${
+              views.cpuUsage ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+            } overflow-hidden`}
+          >
+            <Chart data={cpuUsage} maxDataPoints={10} />
+          </div>
+        )}
       </div>
 
       {/* Toggle RAM usage chart */}
-      <div style={{ height: 120 }}>
-        <h4 id="chart-header-ram" onClick={() => handleClick("ramUsage")}>
+      <div className="my-4">
+        <h4
+          id="chart-header-ram"
+          className="cursor-pointer mx-2 font-bold"
+          onClick={() => handleClick("ramUsage")}
+        >
           RAM Usage
         </h4>
-        {views.ramUsage && <Chart data={ramUsage} maxDataPoints={10} />}
+        {isRendered.ramUsage && (
+          <div
+            className={`transition-all duration-500 ease-in-out ${
+              views.ramUsage ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+            } overflow-hidden`}
+          >
+            <Chart data={ramUsage} maxDataPoints={10} />
+          </div>
+        )}
       </div>
     </div>
   );
