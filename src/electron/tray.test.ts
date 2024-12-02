b5b0d3eae426e4031860db8fec0f51f8fc9f75cb
wjbetech <wjbetech@gmail.com>
2024-12-02 15:01:27 +0900

    Finished writing basic vitest tests for tray functionality

--- src/electron/tray.test.ts
+++ src/electron/tray.test.ts
@@ -1,8 +1,6 @@
-import { expect, test, vi } from "vitest";
+import { expect, Mock, test, vi } from "vitest";
 import { createTray } from "./tray.js";
-import { BrowserWindow, Menu } from "electron";
-import path from "path";
-import { getAssetPath } from "./pathResolver.js";
+import { app, BrowserWindow, Menu } from "electron";
 
 // mock
 vi.mock("electron", () => {
@@ -12,6 +10,10 @@ vi.mock("electron", () => {
     }),
     app: {
       getAppPath: vi.fn().mockReturnValue("/"),
+      dock: {
+        show: vi.fn(),
+      },
+      quit: vi.fn(),
     },
     Menu: {
       buildFromTemplate: vi.fn(),
@@ -24,7 +26,25 @@ const mainWindow = {
   show: vi.fn(),
 } satisfies Partial<BrowserWindow> as any as BrowserWindow;
 
-test("", () => {
+test("Check for tray option (show, quit) /w functionality", () => {
   createTray(mainWindow);
-  expect(Menu.buildFromTemplate).toHaveBeenCalled();
+  const calls = (Menu.buildFromTemplate as any as Mock).mock.calls;
+  const args = calls[0] as Parameters<typeof Menu.buildFromTemplate>;
+  const template = args[0];
+
+  // ensure that the templated menu has the correct # of options
+  expect(template).toHaveLength(2);
+
+  // check the templated menu has the correct options
+  expect(template[0].label).toEqual("Show");
+  expect(template[1].label).toEqual("Quit");
+
+  // menu option 1 "show"
+  template[0]?.click?.(null as any, null as any, null as any);
+  expect(mainWindow.show).toHaveBeenCalled();
+  expect(app.dock.show).toHaveBeenCalled();
+
+  // menu option 2 "quit"
+  template[1]?.click?.(null as any, null as any, null as any);
+  expect(app.quit).toHaveBeenCalled();
 });
